8254c739085008576d3679e27fe7a66ba82580f1
Hoang Hai Nguyen <haij2ee@gmail.com>
2020-12-17 16:42:26 +0100

    command line with the size of the board as an parameter

--- tictactoe/src/main/java/be/nguyenconsulting/demo/tictactoe/TictactoeApplication.java
+++ tictactoe/src/main/java/be/nguyenconsulting/demo/tictactoe/TictactoeApplication.java
@@ -1,13 +1,47 @@
 package be.nguyenconsulting.demo.tictactoe;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.util.Scanner;
+import org.springframework.core.env.Environment;
+
 @SpringBootApplication
-public class TictactoeApplication {
+public class TictactoeApplication implements CommandLineRunner {
+
+	@Autowired
+	private Environment env;
+
+	public  int GRID = 3;
+
+	private static Logger LOG = LoggerFactory.getLogger(TictactoeApplication.class);
 
 	public static void main(String[] args) {
+		LOG.info("STARTING THE APPLICATION");
+
 		SpringApplication.run(TictactoeApplication.class, args);
+
+		Scanner scanner = new Scanner(System.in);
+		String x = scanner.nextLine();
+		LOG.info("x" +x);
+
+		LOG.info("APPLICATION FINISHED");
+
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		LOG.info("EXECUTING : command line runner");
+
+
+		for (int i = 0; i < args.length; ++i) {
+			LOG.info("args[{}]: {}", i, args[i]);
+		}
+	}
+
 
 }
