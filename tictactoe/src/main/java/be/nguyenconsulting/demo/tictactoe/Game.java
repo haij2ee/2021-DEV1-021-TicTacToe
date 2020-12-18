8071a2bc5bfa938be11c543e8a075590beaee419
Hoang Hai Nguyen <haij2ee@gmail.com>
2020-12-18 01:03:57 +0100

    get input n reprint board

--- tictactoe/src/main/java/be/nguyenconsulting/demo/tictactoe/Game.java
+++ tictactoe/src/main/java/be/nguyenconsulting/demo/tictactoe/Game.java
@@ -1,41 +1,50 @@
 package be.nguyenconsulting.demo.tictactoe;
 
 
+import be.nguyenconsulting.demo.tictactoe.entity.Player;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-
-import java.util.Scanner;
-
 
 
 @Component
 public class Game implements CommandLineRunner {
 
 
-    private static final Logger LOG = LoggerFactory.getLogger(Game.class);
     public static final String FILLER = " ";
-
+    private static final Logger LOG = LoggerFactory.getLogger(Game.class);
+    private static int SIZE = 3;
     private String[][] board;
+    private Player currentPlayer = null;
 
-    private Integer SIZE = 3;
+    private Player[] players =
+            {new Player("Humain1", "X"), new Player("Humain2", "O")};
 
 
-    public String[][] getBoard() {
-        return board;
+    public Game(int grid, String player1, String player2) {
+        this(grid, player1);
+        players[1] = new Player(player2, "O");
     }
 
-    public Game(Integer grid) {
+    public Game(Integer grid, String player) {
         SIZE = grid < 3 ? 3 : grid;
         board = new String[SIZE][SIZE];
+        players[0] = new Player(player, "X");
+        currentPlayer = players[0];
         clearBoard();
     }
 
     public Game() {
         board = new String[SIZE][SIZE];
+        players[0] = new Player("Humain1", "X");
+        currentPlayer = players[0];
         clearBoard();
 
+    }
+
+    public String[][] getBoard() {
+        return board;
     }
 
     public void clearBoard() {
@@ -74,21 +83,38 @@ public class Game implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        LOG.info("Game ");
-        printBoard();
+        LOG.debug("Game ");
+        start();
 
-        while (true) {
-            Scanner scanner = new Scanner(System.in);
-            String typedText = scanner.nextLine();
-            LOG.info("Game: " + typedText);
-        }
 
     }
 
 
     public void start() {
-        clearBoard();
+        LOG.debug("START");
+        //       clearBoard();
         printBoard();
+        currentPlayer.playMove(this);
+
     }
+
+    private void changePlayer() {
+        LOG.debug("change Player: currPlayer is " + currentPlayer.getName());
+        currentPlayer = currentPlayer.equals(players[1]) ? players[0] : players[1];
+    }
+
+    public void move(int xPosition, int yPosition) {
+        LOG.debug("move");
+        board[xPosition][yPosition] = currentPlayer.getSymbol();
+        changePlayer();
+        printBoard();
+        currentPlayer.playMove(this);
+    }
+
+}
+
+
+
+
 
 }
--- tictactoe/src/main/java/be/nguyenconsulting/demo/tictactoe/entity/Player.java
+++ tictactoe/src/main/java/be/nguyenconsulting/demo/tictactoe/entity/Player.java
@@ -2,29 +2,28 @@ package be.nguyenconsulting.demo.tictactoe.entity;
 
 
 import be.nguyenconsulting.demo.tictactoe.Game;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.Objects;
 import java.util.Scanner;
-
+import lombok.Getter;
+import lombok.Setter;
 
 @Component
 public class Player {
-    private String name;
 
-    private String symbol;
+    private static final Logger LOG = LoggerFactory.getLogger(Player.class);
+    @Getter  private String name;
+
+
+    @Getter private String symbol;
+
 
     public Player(String name, String symbol) {
         this.name = name;
         this.symbol = symbol;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getSymbol() {
-        return symbol;
     }
 
 
@@ -42,5 +41,17 @@ public class Player {
         return Objects.hash(name, symbol);
     }
 
+    public void playMove(Game game) {
+        LOG.debug("playMove");
+        System.out.println("Enter your x,y positions -> For first row and first column enter 1,1");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+        try {
+            String[] moves = input.split(",");
+            game.move(Integer.parseInt(moves[0]) - 1, Integer.parseInt(moves[1]) - 1);
+        } catch (Exception e) {
+            playMove(game);
+        }
+    }
 
 }
